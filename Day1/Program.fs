251a54b875f03e916c3477fb813764362adf918a
Johannes Scheiermann <johannes.scheiermann@live.de>
2020-12-23 16:14:11 +0100

    Initial commit

---
+++ Day1/Program.fs
@@ -0,0 +1,79 @@
+﻿open Xunit
+open FsUnit
+
+type Combinate<'t> = 't list -> int -> 't list list
+
+let rec combinate: Combinate<'t> =
+    fun inputs size ->
+        if size < 0 then
+            failwith "The pair size can not be negative"
+        else if size = 0 then
+            []
+        else if size = 1 then
+            inputs |> List.map (fun it -> [ it ])
+        else
+            match inputs with
+            | [] -> []
+            | head :: tail ->
+                (combinate tail (size - 1)
+                 |> List.map (fun it -> head :: it))
+                @ combinate tail size
+
+[<Fact>]
+let ``Combining an empty list leads to an empty list`` () =
+    combinate [] 0 |> should be Empty
+    combinate [] 1 |> should be Empty
+
+[<Fact>]
+let ``A PairSize can not be negative`` () =
+    fun () -> combinate [ 1; 2; 3; 4; 5 ] -1 |> ignore
+    |> should throw typeof<System.Exception>
+
+[<Fact>]
+let ``A PairSize of 0 leads to an empty list`` () =
+    combinate [ 1; 2; 3; 4; 5 ] 0 |> should be Empty
+
+[<Fact>]
+let ``With a PairSize of 1, the list items are each returned in their own lists`` () =
+    combinate [ 1; 2; 3; 4; 5 ] 1
+    |> should equal [ [ 1 ]; [ 2 ]; [ 3 ]; [ 4 ]; [ 5 ] ]
+
+[<Fact>]
+let ``Combines correctly for a PairSize of 2`` () =
+    combinate [ 1; 2; 3; 4 ] 2
+    |> should
+        equal
+           [ [ 1; 2 ]
+             [ 1; 3 ]
+             [ 1; 4 ]
+             [ 2; 3 ]
+             [ 2; 4 ]
+             [ 3; 4 ] ]
+
+[<Fact>]
+let ``Combines correctly for PairSizes larger than 2`` () =
+    combinate [ 1; 2; 3; 4; 5 ] 3
+    |> should
+        equal
+           [ [ 1; 2; 3 ]
+             [ 1; 2; 4 ]
+             [ 1; 2; 5 ]
+             [ 1; 3; 4 ]
+             [ 1; 3; 5 ]
+             [ 1; 4; 5 ]
+             [ 2; 3; 4 ]
+             [ 2; 3; 5 ]
+             [ 2; 4; 5 ]
+             [ 3; 4; 5 ] ]
+
+[<EntryPoint>]
+let main argv =
+    let data =
+        System.IO.File.ReadLines "C:\Users\Privat\Repos\AdventOfCode\Day1\Input"
+        |> List.ofSeq
+        |> List.map System.Int32.Parse
+    combinate data 3
+        |> List.find (fun l -> l |> List.sum = 2020 )
+        |> List.reduce (fun a b -> a*b)
+        |> (fun it -> printfn "%i" it)
+    0 // return an integer exit code
---
+++ Day2/Program.fs
@@ -0,0 +1,81 @@
+﻿open System
+open System.Text.RegularExpressions
+
+module Password =
+    type UnvalidatedPassword = UnvalidatedPassword of string
+
+    //type ValidatedPassword =
+    //    private
+    //    | Valid of string
+    //    | NotValid of string
+
+    type PasswordConstraint =
+        { Letter: char
+          MinOccurences: int
+          MaxOccurences: int }
+
+    let isValid1 (UnvalidatedPassword password) constr =
+//        let isValid =
+            password
+            |> Seq.filter (fun c -> c = constr.Letter)
+            |> Seq.length
+            |> fun count ->
+                count
+                >= constr.MinOccurences
+                && count <= constr.MaxOccurences
+//
+//        match isValid with
+//        | true -> Valid password
+//        | false -> NotValid password
+
+    let isValid2 (UnvalidatedPassword password) constr =
+        let count = 
+            ((password.[constr.MinOccurences-1] |> Char.ToString)+(password.[constr.MaxOccurences-1] |> Char.ToString))
+            |> Seq.filter (fun it -> it = constr.Letter)
+            |> Seq.length
+        count = 1
+        
+open Password
+
+type Input =
+    { password: UnvalidatedPassword
+      constr: PasswordConstraint }
+
+let extractInputData (line: string): Input =
+    let r = Regex @"(\d+)-(\d+) (\w): (\w+)"
+    let m = r.Match line
+    let e = (m.Groups.Values |> Seq.toList)
+    { password = UnvalidatedPassword e.[4].Value
+      constr =
+          { Letter = e.[3].Value.[0]
+            MinOccurences = e.[1].Value |> Int32.Parse
+            MaxOccurences = e.[2].Value |> Int32.Parse } }
+
+open Xunit
+open FsUnit
+
+[<Fact>]
+let ``Extracts the input data correctly`` () =
+    extractInputData "10-19 n: nnnnnnnnnnnnnnnnnnnn"
+    |> should
+        equal
+           { password = UnvalidatedPassword "nnnnnnnnnnnnnnnnnnnn"
+             constr =
+                 { Letter = 'n'
+                   MinOccurences = 10
+                   MaxOccurences = 19 } }
+           
+[<Fact>]
+let ``tmp`` () =
+    let data = extractInputData "6-11 z: zzkzpzzzwzf"
+    isValid2 data.password data.constr |> should equal 1
+      
+[<EntryPoint>]
+let main argv =
+    let validPasswordCount =
+        System.IO.File.ReadAllLines "C:\Users\Privat\Repos\AdventOfCode\Day2\Input"
+        |> Seq.map extractInputData
+        |> Seq.filter (fun it -> isValid2 it.password it.constr)
+        |> Seq.length
+    printfn "valid password count: %i" validPasswordCount
+    0 // return an integer exit code
